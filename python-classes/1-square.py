5bcdeb284871a8114cd31989283acc1ce9afabbd
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-23 17:55:49 +0000

    x

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square shape."""
+    def __init__(self, size=0):
+        """returns the size of square."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0)
+~                                
